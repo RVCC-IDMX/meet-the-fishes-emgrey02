2a24bb407f6b154a4b4d84e81bf4da92dc7378d0
Emma Grey <emgrey02@gmail.com>
2022-10-08 17:50:55 -0400

    Design title screen

--- js/animate.js
+++ js/animate.js
@@ -10,6 +10,22 @@ Animate.linear = (x) => x;
 Animate.easeIn  = (x) => x * x;
 Animate.easeOut = (x) => 1 - (1 - x) * (1 - x);
 Animate.easeInOut = (x) => x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
+Animate.easeOutBounce = (x) => {
+    const n1 = 7.5625;
+    const d1 = 2.75;
+
+    if (x < 1 / d1) {
+        return n1 * x * x;
+    } else if (x < 2 / d1) {
+        return n1 * (x -= 1.5 / d1) * x + 0.75;
+    } else if (x < 2.5 / d1) {
+        return n1 * (x -= 2.25 / d1) * x + 0.9375;
+    } else {
+        return n1 * (x -= 2.625 / d1) * x + 0.984375;
+    }
+}
+Animate.easeInOutBack = bezier(0.68,-0.55,0.27,1.55);
+
 
 //Animate to
 Animate.to = function(obj,end) {
